53bbb7f688bf9a3015b44d0f2aa084a5c932d247
PLAYMANWAGON <aironas.keturka@elev.ga.ntig.se>
2021-09-12 19:02:51 +0200

    Mycket text för printstats

--- Pou2/Program.cs
+++ Pou2/Program.cs
@@ -6,7 +6,20 @@ namespace Pou2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Pou 2 ");
+            Console.WriteLine("Welcome to Pou 2 \nPress Enter to continue...");
+
+            Console.ReadLine();
+
+            Character tamagotchi = new Character();
+
+            Console.WriteLine("Name your new digital pet:");
+
+            tamagotchi.name = Console.ReadLine();
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
