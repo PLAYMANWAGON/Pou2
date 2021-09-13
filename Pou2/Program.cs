2bb3fae0aaf8d7f154fd82829bf908c1b4185dff
PLAYMANWAGON <aironas.keturka@elev.ga.ntig.se>
2021-09-13 09:35:31 +0200

    Massa Bra grejer gjort i klassen med metoder

--- Pou2/Program.cs
+++ Pou2/Program.cs
@@ -6,17 +6,51 @@ namespace Pou2
     {
         static void Main(string[] args)
         {
+
+            Character tamagotchi = new Character();
+
+            string choice;
+
             Console.WriteLine("Welcome to Pou 2 \nPress Enter to continue...");
 
             Console.ReadLine();
 
-            Character tamagotchi = new Character();
 
             Console.WriteLine("Name your new digital pet:");
 
             tamagotchi.name = Console.ReadLine();
 
+            Console.WriteLine("What do you want to do with your pet? \n");
 
+            while (tamagotchi.GetAlive())
+            {
+                Console.WriteLine("--Teach,   Feed,   Greet,  Do nothing, Show health--");
+
+                choice = Console.ReadLine().ToLower();
+
+                if (choice == "teach")
+                {
+                    Console.WriteLine($"What word or sentence do you want to teach {tamagotchi.name}?");
+                    tamagotchi.Teach();
+                    tamagotchi.Tick();
+                    Console.Clear();
+                }
+
+                if (choice == "feed")
+                {
+                    tamagotchi.Tick();
+                    tamagotchi.Feed();
+                    Console.WriteLine($"You feed {tamagotchi.name} with food.");
+                    tamagotchi.PrintStats();
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+
+
+            }
+
+            Console.WriteLine($"Oh my god you killed {tamagotchi.name} wtf");
 
 
             Console.ReadLine();
